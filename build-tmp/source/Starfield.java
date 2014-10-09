00ae3cb044b8c78ba4588b661002e9ed8f99c0dd
MagicalLilicorn <lily4ever@msn.com>
2014-10-09 09:42:23 -0700

    Starfield

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,6 +1,22 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 Particle[] poops;
 
-void setup(){	
+public void setup(){	
 	background(0);
 	size(600,600);
 	poops = new Particle[1000];
@@ -9,7 +25,7 @@ void setup(){
 	}
 	poops[999] = new OddballParticle();
 }
-void draw(){
+public void draw(){
 	fill(0,0,0,10);
 	rect(0,0,600,600);
 	for(int i = 0; i<999; i++){
@@ -33,7 +49,7 @@ class NormalParticle implements Particle
 	public void move(){
 		myX += Math.cos(myAngle)*mySpeed;
 		myY += Math.sin(myAngle)*mySpeed;
-		myAngle += .01;
+		myAngle += .01f;
 	}
 	public void show(){
 		noStroke();
@@ -60,7 +76,7 @@ class OddballParticle implements Particle
 	public void move(){
 	   myX = 300 + 200*Math.cos(myAngle);
        myY = 300 + 200*Math.sin(myAngle);
-       myAngle+=.01;
+       myAngle+=.01f;
 	}
 	public void show(){
 		fill(myColor);
@@ -69,3 +85,12 @@ class OddballParticle implements Particle
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
